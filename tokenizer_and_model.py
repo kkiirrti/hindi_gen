8c1e7a8d934e12444bd338b1a00f9565f41a8ded
manash997 <102976851+manash997@users.noreply.github.com>
2023-01-20 17:55:10 +0530

    Revert "Adding custom fill masker code files and req."

--- tokenizer_and_model.py
+++
@@ -1,5 +0,0 @@
-import torch
-from transformers import AutoModelForMaskedLM,AutoTokenizer
-#from transformers import BertForMaskedLM
-tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
-model = AutoModelForMaskedLM.from_pretrained('xlm-roberta-base')
